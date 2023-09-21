e884dfd8fb3c408cc7b37b700b9d45aaf6b1f372
rupesh226 <boyz.226@gmail.com>
2023-09-21 21:55:56 +0800

    shared code with Devise user functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    # Whitelist the following form fields so that we can process them, if coming
+    # from a devise sign up form.
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :phone_number, :full_name])
+    end
+
+
 end
